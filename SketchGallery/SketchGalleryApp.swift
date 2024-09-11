f5bc83de735af170e3484f21935d3c3d6a818662
DiegoRodriguez3D <69866476+DiegoRodriguez3D@users.noreply.github.com>
2024-09-11 13:21:44 +0200

    Initial Commit

---
+++ SketchGallery/SketchGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchGalleryApp.swift
+//  SketchGallery
+//
+//  Created by Diego Rodriguez on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SketchGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
